db6b1c3a0d9e24447b848c38304e495d7d067153
mitani1993 <fytim55@gmail.com>
2022-10-05 20:57:24 +0900

    テストコンポーネント作成

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ComponentTestController extends Controller
 {
-    //
+    public function showComponent1()
+    {
+        return view('tests.component-test1');
+    }
+
+    public function showComponent2()
+    {
+        return view('tests.component-test2');
+    }
 }
