492a8225b61c0f50c6eb9fb4418a78b96c5731e7
mitani1993 <fytim55@gmail.com>
2022-10-23 21:17:18 +0900

    bind, make

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class LifeCycleTestController extends Controller
 {
     //
+    public function showServiceContainerTest()
+    {
+        app()->bind('LifeCycleTest', function () {
+            return 'ライフサイクルテスト';
+        });
+
+        $test = app()->make('LifeCycleTest');
+
+        dd($test, app());
+    }
 }
