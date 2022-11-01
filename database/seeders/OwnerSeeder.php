6c4b76551e743f76a9fc5c85c970fe5328476f71
mitani1993 <fytim55@gmail.com>
2022-11-01 20:38:03 +0900

    owners seeder

--- database/seeders/OwnerSeeder.php
+++ database/seeders/OwnerSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class OwnerSeeder extends Seeder
 {
@@ -13,6 +15,26 @@ class OwnerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('owners')->insert([
+            [
+                'name' => 'test1',
+                'email' => 'test1@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/01/01 11:11:11'
+            ],
+            [
+                'name' => 'test2',
+                'email' => 'test2@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/01/01 11:11:11'
+            ],
+            [
+                'name' => 'test3',
+                'email' => 'test3@test.com',
+                'password' => Hash::make('password123'),
+                'created_at' => '2022/01/01 11:11:11'
+            ],
+
+        ]);
     }
 }
