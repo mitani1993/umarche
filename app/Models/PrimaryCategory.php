ced11d1726a7bf38359c49fd8496280677431629
mitani1993 <fytim55@gmail.com>
2022-11-16 19:24:46 +0900

    リレーション

--- app/Models/PrimaryCategory.php
+++ app/Models/PrimaryCategory.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Models\SecondaryCategory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PrimaryCategory extends Model
 {
     use HasFactory;
+
+    public function secondary()
+    {
+        return $this->hasMany(SecondaryCategory::class);
+    }
 }
