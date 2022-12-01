ee3d41126c789ec4c1e23c0c4afdd87303cfc7ee
mitani1993 <fytim55@gmail.com>
2022-12-01 13:23:45 +0900

    ダミーデータ作成

--- database/factories/StockFactory.php
+++ database/factories/StockFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class StockFactory extends Factory
@@ -14,7 +15,9 @@ class StockFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'product_id' => Product::factory(),
+            'type' => $this->faker->numberBetween(1, 2),
+            'quantity' => $this->faker->randomNumber,
         ];
     }
 }
