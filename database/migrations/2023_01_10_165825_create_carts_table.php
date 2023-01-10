c54f76dfa2ee30fc97c6873c2c8c830de0ffc9e9
mitani1993 <fytim55@gmail.com>
2023-01-10 17:03:50 +0900

    php artisan migrate

--- database/migrations/2023_01_10_165825_create_carts_table.php
+++ database/migrations/2023_01_10_165825_create_carts_table.php
@@ -15,6 +15,15 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('product_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
